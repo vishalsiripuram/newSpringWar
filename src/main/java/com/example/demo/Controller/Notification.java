bce0bbbdbf92fac47f376e7dfc3a74026a2cfc86
Your Name <you@example.com>
2023-03-21 17:36:48 +0530

    commmitted

--- src/main/java/com/example/demo/Controller/Notification.java
+++ src/main/java/com/example/demo/Controller/Notification.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Notification {
     
-    @RequestMapping("/notification")
+    @RequestMapping("/notifications")
     public String notification(){
         return "Notification added";
     }
