5240c3373081e9a1bfdfc9abc7038371f2999a2f
vishalsiripuram <66208358+vishalsiripuram@users.noreply.github.com>
2023-03-21 17:02:16 +0530

    Update controller.java

--- src/main/java/com/example/demo/Controller/controller.java
+++ src/main/java/com/example/demo/Controller/controller.java
@@ -9,6 +9,6 @@ public class controller {
     
     @RequestMapping("/dev")
     public String hello(){
-        return "developer branches webhooks";
+        return "developer branches webhook";
     }
 }
