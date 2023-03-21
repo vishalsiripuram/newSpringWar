076bc3a14550aa62b8aa724c10f00d6481de2be8
Your Name <you@example.com>
2023-03-21 16:32:33 +0530

    commited

--- src/main/java/com/example/demo/Controller/controller.java
+++ src/main/java/com/example/demo/Controller/controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class controller {
     
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String hello(){
         return "hello";
     }
